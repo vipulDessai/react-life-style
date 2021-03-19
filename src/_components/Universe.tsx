6b625daca2516b4b1e1c0b257888ab2cd628048e
Vipul Dessai <badgamerbad@gmail.com>
2021-03-20 00:21:06 +0530

    cube rendered

--- src/_components/Universe.tsx
+++ src/_components/Universe.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import { Galaxy } from '@/_components';
 import { Messages } from '@/_components/Messages';
@@ -46,58 +47,9 @@ export class UniverseComponent extends Component<PropsType, StateType> {
         this.getAllPokemon();
     }
 
-    mount: any;
+    canvas: any;
     componentDidMount() {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setSize(500, 500);
-        this.mount.appendChild( renderer.domElement );
-
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshStandardMaterial( { color: 0x7e31eb } );
-
-        const loader = new GLTFLoader();
-        loader.load(
-            '/assets/3d/cube-color-uv.gltf', 
-            (gltf) => {
-                var mroot = gltf.scene;
-                var bbox = new THREE.Box3().setFromObject(mroot);
-                var cent = bbox.getCenter(new THREE.Vector3());
-                var size = bbox.getSize(new THREE.Vector3());
-
-                //Rescale the object to normalized space
-                var maxAxis = Math.max(size.x, size.y, size.z);
-                mroot.scale.multiplyScalar(1.0 / maxAxis);
-                bbox.setFromObject(mroot);
-                bbox.getCenter(cent);
-                bbox.getSize(size);
-                //Reposition to 0,halfY,0
-                mroot.position.copy(cent).multiplyScalar(-1);
-                mroot.position.y += (size.y * 0.7);
-                scene.add(mroot);
-            },
-            undefined,
-            (error) => {
-                console.log(error);
-            }
-        );
-
-        // const cube = new THREE.Mesh( geometry, material );        
-        // scene.add( cube );
-        const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-        scene.add( light );
-
-        camera.position.z = 2;
-
-        const animate = function () {
-            requestAnimationFrame( animate );
-            // cube.rotation.x += 0.01;
-            // cube.rotation.y += 0.01;
-            // cube.rotation.z += 0.01;
-            renderer.render( scene, camera );
-        };
-        animate();
+        main(this.canvas);
     }
 
     getAllPokemon = async () => {
@@ -245,7 +197,7 @@ export class UniverseComponent extends Component<PropsType, StateType> {
                     this.state.galaxies.map(galaxy => <this.GalaxyComponent key={galaxy.id} galaxy={galaxy} destroyGalaxy={this.destroyGalaxy} />)
                 }
                 </ul>
-                <div ref={ref => (this.mount = ref)}></div>
+                <canvas className="canvas" ref={ref => (this.canvas = ref)}></canvas>
             </section>
         );
     }
@@ -269,3 +221,138 @@ export const Universe = () => {
         return UniverseComponent;
     }
 };
+
+function main(canvas: any) {
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
+    
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
+
+    {
+        const planeSize = 40;
+
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load('/assets/3d/textures/checker.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        texture.repeat.set(repeats, repeats);
+
+        const planeGoe = new THREE.PlaneGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+        });
+
+        const mesh = new THREE.Mesh(planeGoe, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        // scene.add(mesh);
+    }
+
+    {
+        const skyColor = 0xB1E1FF;  // light blue
+        const groundColor = 0xB97A20;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        scene.add(light);
+        scene.add(light.target);
+    }
+
+    // fucntion for city
+    function frameArea(sizeToFitOnScreen: any, boxSize: any, boxCenter: any, camera: any) {
+        const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+        const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+        const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+        // compute a unit vector that points in the direction the camera is now
+        // in the xz plane from the center of the box
+        const direction = (new THREE.Vector3())
+            .subVectors(camera.position, boxCenter)
+            .multiply(new THREE.Vector3(1, 0, 1))
+            .normalize();
+    
+        // move the camera to a position distance units way from the center
+        // in whatever direction the camera was from the center already
+        camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+    
+        // pick some near and far values for the frustum that
+        // will contain the box.
+        camera.near = boxSize / 100;
+        camera.far = boxSize * 100;
+    
+        camera.updateProjectionMatrix();
+    
+        // point the camera to look at the center of the box
+        camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+    }
+
+    {
+        const loader = new GLTFLoader();
+        loader.load(
+            '/assets/3d/cube-color-uv.gltf',
+            // '/assets/3d/city.gltf',
+            (gltf) => {
+                var root = gltf.scene;
+                scene.add(root);
+
+                const box = new THREE.Box3().setFromObject(root);
+
+                const boxSize = box.getSize(new THREE.Vector3()).length();
+                const boxCenter = box.getCenter(new THREE.Vector3());
+
+                frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+                controls.maxDistance = boxSize * 10;
+                controls.target.copy(boxCenter);
+                controls.update();
+            },
+            undefined,
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    const render = function () {
+        if(resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    };
+    
+    requestAnimationFrame(render);
+}
